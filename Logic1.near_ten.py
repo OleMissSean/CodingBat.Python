44546a1cbc098fb09344955f6c55c26a56b4cdcd
Sean Staz <seanstaz52@gmail.com>
2015-06-29 20:14:40 -0500

    Create Logic1.near_ten.py

---
+++ Logic1.near_ten.py
@@ -0,0 +1,15 @@
+"""Python Logic 1 exercise 9.
+
+http://codingbat.com/prob/p165321
+
+Given a non-negative number "num", return True if num is within 2 of a multiple of 10. Note: (a % b) is the remainder of dividing a by b, so (7 % 5) is 2. See also: Introduction to Mod
+
+near_ten(12) → True near_ten(17) → False near_ten(19) → True"""
+
+def near_ten(num):
+  
+    check = num % 10
+    if(check <= 2 or check >= 8):
+        return True
+    else:
+        return False
