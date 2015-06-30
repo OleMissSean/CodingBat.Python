3831fee8737236096e95f3b2d25cab1e7580b081
Sean Staz <seanstaz52@gmail.com>
2015-06-29 20:34:01 -0500

    Create String1.hello_name.py

---
+++ String1.hello_name.py
@@ -0,0 +1,11 @@
+"""Python String 1 exercise 1.
+
+http://codingbat.com/prob/p115413
+
+Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+hello_name('Bob') → 'Hello Bob!' hello_name('Alice') → 'Hello Alice!' hello_name('X') → 'Hello X!'"""
+
+def hello_name(name):
+  
+    return ('Hello %s!' % name)
